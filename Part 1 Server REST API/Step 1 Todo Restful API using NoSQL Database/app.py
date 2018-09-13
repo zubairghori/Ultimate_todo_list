f2211f81163566f39a8745de581f3b9434ea5a87
Mohsin <mohsinhussain1993@gmail.com>
2018-09-13 18:51:40 +0500

    connected with remote database

--- Part 1 Server REST API/Step 1 Todo Restful API using NoSQL Database/app.py
+++ Part 1 Server REST API/Step 1 Todo Restful API using NoSQL Database/app.py
@@ -1,3 +1,9 @@
-from flask import Flask, render_template_string
-from flask_mongoengine import MongoEngine
-from flask_user import login_required, UserManager, UserMixin
+from flask import Flask,jsonify,request
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'todorest' 
+app.config['MONGO_URI'] = 'mongodb://mohsin:password1@ds123852.mlab.com:23852/todorest'
+
+mongo = PyMongo(app)
