03940c38786422ef4fe11970e017a9203f603735
Mohsin <mohsinhussain1993@gmail.com>
2018-09-13 18:58:03 +0500

    GET, PUT and Delete method implemented

--- Part 1 Server REST API/Step 1 Todo Restful API using NoSQL Database/app.py
+++ Part 1 Server REST API/Step 1 Todo Restful API using NoSQL Database/app.py
@@ -9,6 +9,18 @@ app.config['MONGO_URI'] = 'mongodb://mohsin:password1@ds123852.mlab.com:23852/to
 mongo = PyMongo(app)
 
 
+@app.route('/todo/api/v1/tasks',methods=['GET'])
+def all_tasks():
+    task = mongo.db.tasks
+
+    output=[]
+
+    for a in task.find():
+
+        output.append({'task_id':a['task_id'],'task_title':a['task_title'],'task_description': a['task_description'],'task_done': a['task_done']})
+
+    return jsonify({'result':output})
+
 @app.route('/todo/api/v1/tasks', methods=['POST'])
 def add():
     task = mongo.db.tasks
@@ -19,6 +31,28 @@ def add():
     task.insert({'task_id':task_id, 'task_title':task_title, 'task_description':task_description, 'task_done':task_done})
         
     return "task inserted"
+
+
+
+@app.route('/todo/api/v1/tasks/<task_id>',methods=['DELETE'])
+def delete_task(task_id):
+    task = mongo.db.tasks
+    a={'task_id':task_id}
+
+    task.delete_one(a)
+    return "task deleted"
+
+@app.route('/todo/api/v1/tasks/<task_id>',methods=['PUT'])
+def update_task_title(task_id):
+    task = mongo.db.tasks
+
+    update=task.find_one({'task_id':task_id})
+    task_title = request.json['task_title']
+
+    update['task_title']= task_title
+
+    task.save(update)
+    return 'title updated'
 
 if __name__ == '__main__':
     app.run(debug=True)
