02bd7548d1ffaa9cc2270bbb43efc4e456ee8b95
Mohsin <mohsinhussain1993@gmail.com>
2018-09-13 18:55:09 +0500

    POST method implemented

--- Part 1 Server REST API/Step 1 Todo Restful API using NoSQL Database/app.py
+++ Part 1 Server REST API/Step 1 Todo Restful API using NoSQL Database/app.py
@@ -7,3 +7,18 @@ app.config['MONGO_DBNAME'] = 'todorest'
 app.config['MONGO_URI'] = 'mongodb://mohsin:password1@ds123852.mlab.com:23852/todorest'
 
 mongo = PyMongo(app)
+
+
+@app.route('/todo/api/v1/tasks', methods=['POST'])
+def add():
+    task = mongo.db.tasks
+    task_id = request.json['task_id']
+    task_title = request.json['task_title']
+    task_description = request.json['task_description']
+    task_done = request.json['task_done']
+    task.insert({'task_id':task_id, 'task_title':task_title, 'task_description':task_description, 'task_done':task_done})
+        
+    return "task inserted"
+
+if __name__ == '__main__':
+    app.run(debug=True)
