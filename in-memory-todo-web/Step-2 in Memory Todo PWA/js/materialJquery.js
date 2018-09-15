5b7626c0fc05750d2a6f59d2b15cb465d543728d
soha-moosa <sohamoosa153@gmail.com>
2018-09-16 03:26:26 +0500

    Add Task Floating Button with Tooltipped Property

--- in-memory-todo-web/Step-2 in Memory Todo PWA/js/materialJquery.js
+++ in-memory-todo-web/Step-2 in Memory Todo PWA/js/materialJquery.js
@@ -10,3 +10,14 @@ $(document).ready(function(){
 $(document).ready(function () {
     $('.floating-action-btn').floatingActionButton();
 });
+
+
+// Add Task Floating Button with tooltipped property
+
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
+
+$(document).ready(function () {
+    $('.tooltipped').tooltip();
+});
