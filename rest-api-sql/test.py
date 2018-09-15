6e5818b78b78081a8943ed0e7ce02efb6492f781
Maryam Bilal <mariambilal6@gmail.com>
2018-09-15 06:01:20 -0700

    test case of view function

--- rest-api-sql/test.py
+++ rest-api-sql/test.py
@@ -16,10 +16,17 @@ class apiTest(unittest.TestCase):
         response = self.appli.post('/todo/api/v1.0/task/add',data=json.dumps(dict({'title':'ALi', 'description' : 'Ahmed'})), content_type='application/json')
         self.assertEqual(response.status_code, 200, "OK")
 
-  
+    # test function for view
+    def test_view(self):
+        response = self.viewapp.get('/todo/api/v1.0/task/view', content_type = 'application/json')
+        self.assertEqual(response.status_code,200)
+
+
     def test_update(self):   # test function for update 
         response = self.appli.put('/todo/api/v1.0/task/update/30',data=json.dumps(dict({'title':'Ali', 'description' : 'Ali'})), content_type = 'application/json')
         self.assertEqual(response.status_code, 200, "OK")
+
+    
 
 if __name__ == "__main__":
     unittest.main()
