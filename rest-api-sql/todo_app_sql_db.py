858b747d2123c004430e3971df0419df09053437
Maryam Bilal <mariambilal6@gmail.com>
2018-09-15 05:49:45 -0700

    view function
    a function to view all data from database

--- rest-api-sql/todo_app_sql_db.py
+++ rest-api-sql/todo_app_sql_db.py
@@ -17,6 +17,7 @@ class Todo_App(database.Model):
 
 database.create_all() # creating tables
 
+# rounting path for add task
 @app.route('/todo/api/v1.0/task/add' , methods=['POST'])
 def add():              # add function.
     added = request.get_json()
@@ -26,4 +27,32 @@ def add():              # add function.
     database.session.add(add_task) # insert data query
     database.session.commit()
     return jsonify({'prompt':'Added'}) # return data in jsonify format
+
+
+
+#rounting path for view
+@app.route('/todo/api/v1.0/task/view' , methods=['GET'])
+
+#funtion for view task
+def task_view():
+    task = Todo_App.query.all() #fetching data for db
+    list_of_task =[] #empty array
+    """
+    iterating over the 'task' to get each column value and 
+    appening it into the 'list_of_task'. then jsonify the 
+    final result that will be show on the screen. 
+    """
+    for item in task:
+        item_data={}
+        item_data['id'] = item.id
+        item_data['title'] = item.title
+        item_data['description'] = item.description
+        item_data['done'] = item.done
+        list_of_task.append(item_data) #appending each into the dicnary opject
+
+    return jsonify({'task': list_of_task}) #jsonify the result
+
+
+if __name__ == "main":
+    app.run(debug = True, port = 8000)
 
