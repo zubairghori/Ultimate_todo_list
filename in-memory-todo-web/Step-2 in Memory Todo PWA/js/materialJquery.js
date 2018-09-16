1f88ea5b794aefa22256035be49e3edb3b710d94
soha-moosa <sohamoosa153@gmail.com>
2018-09-17 02:41:56 +0500

    Updated modal and floating action button's jquery

--- in-memory-todo-web/Step-2 in Memory Todo PWA/js/materialJquery.js
+++ in-memory-todo-web/Step-2 in Memory Todo PWA/js/materialJquery.js
@@ -1,18 +1,22 @@
 
-// side navbar
+// ===== side nav ===== //
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
 
-// Floating Action Button
+// ===== Floating Action Button ===== //
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.floating-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+});
 
 $(document).ready(function () {
     $('.floating-action-btn').floatingActionButton();
 });
 
-
-// Add Task Floating Button with tooltipped property
+// ===== Add Task Fixed Action Button with tooltipped property ===== //
 
 $(document).ready(function () {
     $('.fixed-action-btn').floatingActionButton();
@@ -21,3 +25,9 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.tooltipped').tooltip();
 });
+
+// ===== Modal ===== //
+ 
+$(document).ready(function(){
+    $('.modal').modal();
+  });
