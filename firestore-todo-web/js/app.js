373fe3d41c317d0ec5141480c11cb26b3f88945c
soha-moosa <sohamoosa153@gmail.com>
2018-09-19 01:18:14 +0500

    Delete data functionality added using Google’s Firebase Firestore Database

--- firestore-todo-web/js/app.js
+++ firestore-todo-web/js/app.js
@@ -11,66 +11,74 @@ var config = {
 firebase.initializeApp(config);
 const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true })
-let todoArray = [];
 
-db.collection('todos').get().then((snapshot) => {
-    console.log(snapshot.docs);
+// ===== Reading all from DATABASE =====
+
+db.collection('todos').onSnapshot((snapshot) => {
+
+    let todoArray = [];
     snapshot.docs.forEach(doc => {
-
-        todoArray.push(doc.data());
+        let todo = {
+            id: doc.id,
+            ...doc.data()
+        };
+        todoArray.push(todo);
     });
-    getDataFromDatabase(todoArray);
+    printToDOM(todoArray);
+
 })
 
+// ===== Printing to DOM ===== 
 
-let getDataFromDatabase = (doc) => {
-    console.log(doc);
+let printToDOM = (doc) => {
     let todoListCards = document.getElementById('todoListCards');
     todoListCards.innerHTML = "";
     doc.map((item) => {
-
         $(document).ready(function () {
             $('.floating-action-btn').floatingActionButton();
         });
-        console.log(item);
+
+        $(document).ready(function () {
+            $('.tooltipped').tooltip();
+        });
+        
         todoListCards.innerHTML +=
             `
-                <div class"container">
-                <div class="row">
-                <div class="col s6 m12 l12">
-                <div class="card grey lighten-5">
-                 
-                <div class="card-content black-text z-depth-4 hoverable">
-                <span class="card-title">
-                    <h5>${item.Title}</h5>
-                </span>
-                <h6>${item.Description}</h6>
-                <!-- Floating Action Button at the end of card-->
-                <div class="floating-action-btn">
-                    <a class="btn-floating btn-small blue-grey lighten-2 right">
-                        <i class="large material-icons">more_vert</i>
-                    </a>
-                    <ul>
-                        <li>
-                            <a class="btn-floating btn red right tooltipped modal-trigger" href="#modal2" data-delay="1000" data-tooltip="Edit" data-position="top">
-                                <i class="material-icons">mode_edit</i>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="btn-floating yellow right darken-1 tooltipped" data-delay="1000" data-tooltip="Delete" data-position="top">
-                                <i class="material-icons">delete</i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <div class"container">
+        <div class="row">
+        <div class="col s6 m12 l12">
+        <div class="card grey lighten-5">
+        
+        <div class="card-content black-text z-depth-4 hoverable">
+        <span class="card-title">
+        <h5>${item.Title}</h5>
+        </span>
+        <h6>${item.Description}</h6>
+        <!-- Floating Action Button at the end of card-->
+        <div class="floating-action-btn">
+        <a class="btn-floating btn-small blue-grey lighten-2 right">
+        <i class="large material-icons">more_vert</i>
+        </a>
+        <ul>
+        <li>
+        <a class="btn-floating btn red right tooltipped modal-trigger" href="#modal2" data-delay="1000" data-tooltip="Edit" data-position="top">
+        <i class="material-icons">mode_edit</i>
+        </a>
+        </li>
+        <li>
+        <button onclick="deleteThisTaskFromDatabase('${item.id}');" class="btn-floating yellow right darken-1 tooltipped" data-delay="1000" data-tooltip="Delete" data-position="top">
+        <i class="material-icons">delete</i>
+        </button>
+        </li>
+        </ul>
         </div>
         </div>
-    </div>
-</div>
-`;
+        </div>
+        </div>
+        </div>
+        </div>
+        `;
     })
-
 }
 
 // ====== Saving Data =====
@@ -78,16 +86,22 @@ let getDataFromDatabase = (doc) => {
 let addTaskForm = document.getElementById('addTaskForm');
 addTaskForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('Inside addTaskForm')
     let Title = document.getElementById("Title").value;
     let Description = document.getElementById("Description").value;
+    document.getElementById('Title').value = "";
+    document.getElementById('Description').value = "";
     db.collection('todos').add({
         Title,
         Description,
         taskDone: false
     })
-    Title.value = "";
-    Description.value = "";
+
 })
 
 
+// ===== Deleting Data =====
+
+let deleteThisTaskFromDatabase = (id) => {
+    db.collection('todos')
+        .doc(id).delete();
+};
