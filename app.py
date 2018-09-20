19c5594b05b37ed41b91f22f357ad1ccf41e51a2
Mohsin <mohsinhussain1993@gmail.com>
2018-09-20 21:17:35 +0500

    CORS file added for api

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask,jsonify,request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 app.config['MONGO_DBNAME'] = 'todorest' 
 app.config['MONGO_URI'] = 'mongodb://mohsin:password1@ds123852.mlab.com:23852/todorest'
 
