93e0dd2e65bd41f62770df37769c6d571a67e7d2
AreebaAftab <areebanovember@hotmail.com>
2018-10-01 01:09:45 +0500

    delete test case 1

--- rest-api-sql/tests/delete_test.py
+++ rest-api-sql/tests/delete_test.py
@@ -11,6 +11,11 @@ class apiTest(unittest.TestCase):
 
     def tearDown(self):
         pass
+# -------------------------- unit test for successfully deleted data -----------------------------------
+
+    def test_delete(self):
+        response = self.appli.delete('/todo/api/v1.0/task/delete/30',data=json.dumps(dict({'title':'Ali', 'description' : 'Ali'})), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200, "OK")
 
 
 
