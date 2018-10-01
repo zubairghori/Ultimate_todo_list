7070b0e292f6460468b97d74d03330505c92da82
Mohsin <mohsinhussain1993@gmail.com>
2018-10-01 14:44:23 +0500

    added tests

--- react-todo-rest-api/src/App.test.js
+++ react-todo-rest-api/src/App.test.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Titles from "./components/Titles";
+import Edit from "./components/Edit";
+import Form from "./components/Form";
+import Remove from "./components/Remove";
+import Task from "./components/Task";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Titles />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Edit />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Form />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Remove />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Task />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
